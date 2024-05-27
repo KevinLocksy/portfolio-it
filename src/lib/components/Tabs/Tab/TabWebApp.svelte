93f11481d04c4cf9583a6d9adcc6b648a23665d1
Kevin Locksy <nhan.kev@gmail.com>
2024-05-28 00:20:04 +0200

    Add a card in tabWebapp

--- src/lib/components/Tabs/Tab/TabWebApp.svelte
+++ src/lib/components/Tabs/Tab/TabWebApp.svelte
@@ -1,9 +1,12 @@
 <script>
-  import Card from '$components/Card.svelte'
+  import Card from '$components/Card.svelte';
+  import Projects from '$assets/projects/webapp/webapp.projects.json';
+  const webapps = Projects.webapp;
 </script>
-
 <div class="tab">
-  <Card></Card>
+  {#each webapps as webapp}
+    <Card project={webapp.project} standfirst={webapp.standfirst} summary={webapp.summary} description={webapp.description} features={webapp.features} git={webapp.git} demo={webapp.git} stack={webapp.stack} img={webapp.img}/>
+  {/each}
 </div>
 
 <style>
