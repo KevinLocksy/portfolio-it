3623721e574e4a3c2104b6db817e7f995d245593
Kevin Locksy <nhan.kev@gmail.com>
2024-06-03 22:17:25 +0200

    Change components names

--- src/lib/components/Tabs/TabsItems.js
+++ src/lib/components/Tabs/TabsItems.js
@@ -1,38 +1,38 @@
-import TabWebApp from '$components/Tabs/Tab/TabWebApp.svelte';
-import TabMobileApp from '$components/Tabs/Tab/TabMobileApp.svelte';
-import TabAlgo from '$components/Tabs/Tab/TabAlgo.svelte';
-import TabDocTemplate from '$components/Tabs/Tab/TabDocTemplate.svelte';
-import TabSnippet from '$components/Tabs/Tab/TabSnippet.svelte';
+import WebApp from '$components/Tabs/Tab/WebApp.tab.svelte';
+import MobileApp from '$components/Tabs/Tab/MobileApp.tab.svelte';
+import Algo from '$components/Tabs/Tab/Algo.tab.svelte';
+import DocTemplate from '$components/Tabs/Tab/DocTemplate.tab.svelte';
+import Snippet from '$components/Tabs/Tab/Snippet.tab.svelte';
 
 const TabsItems = [
   {
     "id":0,
     "label":"Web applications",
-    "component":TabWebApp,
+    "component":WebApp,
     "comments":"web applications in svelte/pure css..."
   },
   {
     "id":1,
     "label":"Mobile applications",
-    "component":TabMobileApp,
+    "component":MobileApp,
     "comments":"Mobile applications in react native/flutter..."
   },
   {
     "id":2,
     "label":"Algorithms",
-    "component":TabAlgo,
+    "component":Algo,
     "comments":"Algorithms in python, ml..."
   },
   {
     "id":3,
     "label":"Document templates",
-    "component":TabDocTemplate,
+    "component":DocTemplate,
     "comments":"Document templates in latex/figma css..."
   },
   {
     "id":4,
     "label":"Snippets",
-    "component":TabSnippet,
+    "component":Snippet,
     "comments":"Snippet codes in every language"
   }
 ]
