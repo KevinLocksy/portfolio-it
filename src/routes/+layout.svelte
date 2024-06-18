10f9f60cb47c3daa9d080a9f65890aef8678cc9a
Kevin Locksy <nhan.kev@gmail.com>
2024-06-18 14:31:46 +0200

    Restructure code to have header-footer in all the pages including error page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,117 @@
 <script>
   import {i18n} from '$utils/i18n.utils.js';
+  import {toggleTheme} from '$utils/theme.utils.js';
+  import {i18nStore} from '$store/i18n.store.js';
+  import {isDarkTheme} from '$store/theme.store.js';
+
+  import PERSONAL_INFO from '$config/personalInfo.conf.json';
+  import LANG from "$config/i18n.conf.json";
+	import DropdownList from '$components/DropdownList.svelte';
+
+  const LOGO = PERSONAL_INFO.logo;
+  const LANGUAGE = LANG.language;
+
+  const onLangChange = async (e) => {
+    const newLang = e.target.value;
+    await i18n.switchLang(newLang);
+  }
 </script>
 
 {#await i18n.init()}
   <div>wait</div>
 {:then _}
-  <slot />
+  <header>
+    <div id='trademark'>
+      <div class='color-scheme' role='button' on:click={toggleTheme} on:keyup tabindex="0">
+        <img class='logo locksy' src={isDarkTheme? LOGO.img.dark : LOGO.img.light} alt='{LOGO.alt}' title='{LOGO.title}' />
+      </div>
+      <h1 class='title'>{PERSONAL_INFO.pseudo} - IT Portfolio</h1>
+    </div>
+
+    <div class='lang-dropdown'>
+      <DropdownList options={LANGUAGE} defaultValue={$i18nStore} onClick={onLangChange}/>
+    </div>
+  </header>
+
+  <main>
+    <slot />
+  </main>
+
+  <footer>
+    <h4 id='credits'>{$i18n.footer.credits}</h4>
+    <h4 id='contact'>{$i18n.footer.contact}</h4>
+    <h4 id='date'>Date</h4>
+  </footer>
 {/await}
+
+<style>
+  header, footer{
+    max-width:100%;
+    min-height:50px;
+    align-items: center;
+    box-shadow: 0px 1px 5px var(--box-shadow-primary);
+    background-color: var(--background-secondary);
+    padding: 0.5em 1em;
+  }
+  /**
+  * Structure
+  */
+  header{
+    display: flex;
+    justify-content:space-between;
+    position: sticky;
+    top:0;
+    max-height:75px;
+  }
+  main{
+    flex:auto;
+    background: radial-gradient(
+      800px circle at calc(var(--x) * 1px) calc(var(--y) * 1px),
+      var(--background-halo-cursor-primary),
+      transparent 90%
+      );
+  }
+  footer{
+    display: grid;
+    grid-template-areas: "credits contact date";
+    grid-template-columns: repeat(3,1fr);
+    position: sticky;
+    bottom:0;
+  }
+  /**
+  * Header elements
+  */
+  div#trademark{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  div.color-scheme{
+    cursor: pointer;
+  }
+  div.lang-dropdown{
+    position: relative;
+    height: inherit;
+    width: clamp(3em,4em + 10vw,15em);
+  }
+  /**
+  * Footer elements
+  */
+  footer>*{
+    /** To be generic */
+    &:first-child{
+      grid-column: 1;
+      grid-area: credits;
+    }
+    &:nth-child(2){
+      grid-column: 2;
+      grid-area: contact;
+      justify-self: center;
+    }
+    &:last-child{
+      grid-column: 3;
+      grid-area: date;
+      justify-self: end;
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,167 +5,63 @@
 
 <script>
   import SOCIAL_MEDIA from '$config/socialmedia.conf.json';
-  import PERSONAL_INFO from '$config/personalInfo.conf.json';
   import TECH_STACK from '$config/techStack.conf.json';
-  import LANG from "$config/i18n.conf.json";
   import {setCursorPositions} from '$utils/cursor.utils.js';
   import {typeText} from '$utils/textTyper.utils.js';
-  import {toggleTheme} from '$utils/theme.utils.js';
   import {isDarkTheme} from '$store/theme.store.js';
   import {i18n} from '$utils/i18n.utils.js';
-  import {i18nStore} from '$store/i18n.store.js';
   import Tabs from '$components/Tabs/Tabs.svelte';
   import Model3d from '$components/Model3d/Model3d.svelte';
-	import DropdownList from '$components/DropdownList.svelte';
-  const LOGO = PERSONAL_INFO.logo;
-  const LANGUAGE = LANG.language;
-  //https://svelte.dev/repl/de39de663ef2445b8fe17b79c500013b?version=3.33.0 i18n
-  //https://codepen.io/davidkpiano/pen/gOoNZNe halo cursor
-  //https://threejs.org/docs/index.html#manual/en/introduction/Installation 3D
 
   setCursorPositions();
-  const onLangChange = async (e) => {
-    const newLang = e.target.value;
-    await i18n.switchLang(newLang);
-  }
 </script>
 
-<header>
-  <div id='trademark'>
-    <div class='color-scheme' role='button' on:click={toggleTheme} on:keyup tabindex="0">
-      <img class='logo locksy' src={isDarkTheme? LOGO.img.dark : LOGO.img.light} alt='{LOGO.alt}' title='{LOGO.title}' />
-    </div>
-    <h1 class='title'>{PERSONAL_INFO.pseudo} - IT Portfolio</h1>
-  </div>
-
-  <div class='lang-dropdown'>
-    <DropdownList options={LANGUAGE} defaultValue={$i18nStore} onClick={onLangChange}/>
-  </div>
-</header>
-  
-<main>
-  <div id='main-wrapper'> <!-- wrapper if i want to make a component out of it: it is easier for the css, etc-->
-    <section id="presentation">
-      <div id='pres-desc'>
-        <div id='pres-desc-text'>
-          <p> {@html $i18n.pres.title}</p>
-          <p> {@html $i18n.pres.desc}</p>
-        </div>
-        <div id="pres-desc-social">
-          {#each SOCIAL_MEDIA.socialmedia as { url, img, alt, title }}
-            <div class="socialMedia">
-              <a href={url}>
-                <img class='logo' src={$isDarkTheme ? img.dark : img.light} alt='{alt}' title='{title}' />
-              </a>
-            </div>
-          {/each}
-        </div>
+<div id='main-wrapper'> <!-- wrapper if i want to make a component out of it: it is easier for the css, etc-->
+  <section id="presentation">
+    <div id='pres-desc'>
+      <div id='pres-desc-text'>
+        <p> {@html $i18n.pres.title}</p>
+        <p> {@html $i18n.pres.desc}</p>
       </div>
-      <div id='pres-3d'>
-        <Model3d />
-      </div>
-    </section>
-    
-    <section id="techStack">
-      <div id="techStack-wrapper">
-        <h2 use:typeText class="font-tech">{$i18n.techStack.desc}</h2>
-        <div id="techStack-list">
-          {#each TECH_STACK.techStack as {name, img}}
-          <div class="techStack-logo-container"  name='{name}'>
-            <img class='techStack-logo-img logo' src={img.path} alt='{img.alt}' title='{img.title}' />
-            <h4 class="techStack-logo-caption">{name}</h4>
+      <div id="pres-desc-social">
+        {#each SOCIAL_MEDIA.socialmedia as { url, img, alt, title }}
+          <div class="socialMedia">
+            <a href={url}>
+              <img class='logo' src={$isDarkTheme ? img.dark : img.light} alt='{alt}' title='{title}' />
+            </a>
           </div>
-          {/each}
-        </div>
+        {/each}
       </div>
-    </section>
+    </div>
+    <div id='pres-3d'>
+      <Model3d />
+    </div>
+  </section>
+  
+  <section id="techStack">
+    <div id="techStack-wrapper">
+      <h2 use:typeText class="font-tech">{$i18n.techStack.desc}</h2>
+      <div id="techStack-list">
+        {#each TECH_STACK.techStack as {name, img}}
+        <div class="techStack-logo-container"  name='{name}'>
+          <img class='techStack-logo-img logo' src={img.path} alt='{img.alt}' title='{img.title}' />
+          <h4 class="techStack-logo-caption">{name}</h4>
+        </div>
+        {/each}
+      </div>
+    </div>
+  </section>
 
-    <section id="projects">
-      <Tabs />
-    </section>
+  <section id="projects">
+    <Tabs />
+  </section>
 
-    <section id="art">
-      
-    </section>
-  </div>
-</main>
-
-  <footer>
-    <h4 id='credits'>{$i18n.footer.credits}</h4>
-    <h4 id='contact'>{$i18n.footer.contact}</h4>
-    <h4 id='date'>Date</h4>
-  </footer>
+  <section id="art">
+    
+  </section>
+</div>
 
 <style>
-  /* @import url("./styles.css"); */
-  header, footer{
-    max-width:100%;
-    min-height:50px;
-    align-items: center;
-    box-shadow: 0px 1px 5px var(--box-shadow-primary);
-    padding: 0.5em 1em;
-  }
-  section{
-    margin: 1em;
-  }
-  /**
-  * Structure
-  */
-  header{
-    display: flex;
-    justify-content:space-between;
-    position: sticky;
-    top:0;
-    max-height:75px;
-  }
-  main{
-    background: radial-gradient(
-      800px circle at calc(var(--x) * 1px) calc(var(--y) * 1px),
-      var(--background-halo-cursor-primary),
-      transparent 90%
-      );
-  }
-  footer{
-    display: grid;
-    grid-template-areas: "credits contact date";
-    grid-template-columns: repeat(3,1fr);
-  }
-  /**
-  * Header elements
-  */
-  div#trademark{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-  }
-  div.color-scheme{
-    cursor: pointer;
-  }
-  div.lang-dropdown{
-    position: relative;
-    height: inherit;
-    width: clamp(3em,4em + 10vw,15em);
-  }
-  /**
-  * Footer elements
-  */
-  footer>*{
-    /** To be generic */
-    &:first-child{
-      grid-column: 1;
-      grid-area: credits;
-    }
-    &:nth-child(2){
-      grid-column: 2;
-      grid-area: contact;
-      justify-self: center;
-    }
-    &:last-child{
-      grid-column: 3;
-      grid-area: date;
-      justify-self: end;
-    }
-  }
   /**
   * Main
   */
@@ -173,19 +69,8 @@
     margin: 0 auto;
     max-width: 1440px;
   }
-  /**
-  * Logo
-  */
-  .logo, .socialMedia {
-    width:clamp(15px,15px + 5vw,50px);
-    height: auto;
-  }
-  .socialMedia {
-    margin: 0 0.5em;
-  }
-  .logo.locksy{
-    border-radius: 50px;
-    width: clamp(25px,50px + 1vw,50px);
+  section{
+    margin: 1em;
   }
   /**
   * Presentation section
