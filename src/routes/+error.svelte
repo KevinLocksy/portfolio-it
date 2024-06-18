cfb1e51af39b783beab8eebc7e8422933b0ad95e
Kevin Locksy <nhan.kev@gmail.com>
2024-06-18 14:30:57 +0200

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+  <title>Error - Kevin Locksy - IT Portfolio</title>
+  <meta name="description" content="Kevin Locksy - Official IT Portfolio."/>
+</svelte:head>
+
+<div id='error-page'>
+  Error 404
+
+  <div id='nav-back'>
+    <div id='page-main'>
+      <a href="/">
+        Go back to the main page
+      </a>
+    </div>
+    <div id='page-3d'>
+    </div>
+  </div>
+</div>
+
+<style>
+
+</style>
